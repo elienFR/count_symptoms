f4c45927c334f80ae87db21bfe7446ebf59d58c0
Elien <elienferre@yahoo.fr>
2021-10-04 11:57:27 +0200

    modifiation added to the ReadMethod

--- src/com/hemebiotech/analytics/ISymptomReader.java
+++ src/com/hemebiotech/analytics/ISymptomReader.java
@@ -6,15 +6,18 @@ import java.util.List;
  * Anything that will read symptom data from a source
  * The important part is, the return value from the operation, which is a list of strings,
  * that may contain many duplications
- * 
+ *
  * The implementation does not need to order the list
- * 
+ *
  */
 public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 *
+	 * @return a raw listing of all Symptoms unically picked from a data  file
+	 * withoput any duplicates.
+	 * It also count the number of unique symptoms found.
+	 *
 	 */
-	List<String> GetSymptoms ();
+	List<String> enumerateSymptoms(boolean verbose);
 }
--- src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -13,35 +13,77 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+	private List<String> symptomsEnumeration;
+
 	/**
-	 * 
+	 *
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
-	
+
+
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
+	public List<String> enumerateSymptoms(boolean verboseMode) {
+
+		System.out.println("Starting enumerating Symptoms...");
+
+		this.symptomsEnumeration = new ArrayList<String>();
+
+		if (this.filepath != null) {
 			try {
+				boolean alreadyExists = false;		//Is the current line already exists in this.symptomsEnumeration
+				boolean firstInjection = true;		//Used to increment the this.symptomsEnumeration array list for the first time
+
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
 				String line = reader.readLine();
-				
+
 				while (line != null) {
-					result.add(line);
+					//This part deals with empty lines to prevent them from being treated
+					if (!line.equals("")) {
+						if(firstInjection) {
+							this.symptomsEnumeration.add(line);
+							firstInjection = false;
+						}
+
+						else {
+							for (int i = 0; i < this.symptomsEnumeration.size(); i++) {
+								if (line.equals(this.symptomsEnumeration.get(i))) {
+									alreadyExists = true;
+									break;
+								}
+
+								else {
+									alreadyExists = false;
+								}
+							}
+
+							if (!alreadyExists){
+								this.symptomsEnumeration.add(line);
+							}
+						}
+					}
+
 					line = reader.readLine();
 				}
+
 				reader.close();
+				System.out.println("Enumeration ended successfully.");
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		return result;
+
+		if(verboseMode) {
+			System.out.println("Here is the list of unique symptoms extracted from the file !");
+			for(int i=0; i<this.symptomsEnumeration.size();i++){
+				System.out.println(i + " : " + this.symptomsEnumeration.get(i));
+			}
+			System.out.println("There are " + this.symptomsEnumeration.size() + " different symptoms in total.");
+		}
+		return this.symptomsEnumeration;
 	}
 
 }
